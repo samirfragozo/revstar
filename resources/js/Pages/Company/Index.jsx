86193a3d28fa594e8cac6e09e9f828d4c1d8668b
Samir Fragozo <samirfragozob@gmail.com>
2023-05-18 02:54:14 -0500

    Company - table partial

--- resources/js/Pages/Company/Index.jsx
+++ resources/js/Pages/Company/Index.jsx
@@ -1,5 +1,6 @@
 import AuthenticatedLayout   from '@/Layouts/AuthenticatedLayout';
 import {Head, Link, useForm} from '@inertiajs/react';
+import Table                 from '@/Pages/Company/Partials/Table.jsx';
 
 export default function Index({
     auth,
@@ -50,49 +51,16 @@ export default function Index({
                             </Link>
                         </div>
 
-                        <div className="mt-4 shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
-                            <table className="min-w-full divide-y divide-gray-200">
-                                <thead className="bg-gray-50">
-                                <tr>
-                                    <Th>Nit</Th>
-                                    <Th>Nombre</Th>
-                                    <Th>Dirección</Th>
-                                    <Th>Teléfono</Th>
-                                    <Th/>
-                                </tr>
-                                </thead>
-                                <tbody className="bg-white divide-y divide-gray-200">
-                                {companies.map((company) => (
-                                    <tr key={company.id}>
-                                        <Td>{company.nit}</Td>
-                                        <Td>{company.name}</Td>
-                                        <Td>{company.address}</Td>
-                                        <Td>{company.phone}</Td>
-                                        <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-                                            <Link
-                                                href={`/companies/${company.id}`}
-                                                className="text-blue-600 hover:text-blue-900 mr-2"
-                                            >
-                                                Ver
-                                            </Link>
-                                            <Link
-                                                href={`/companies/${company.id}/edit`}
-                                                className="text-green-600 hover:text-green-900 mr-2"
-                                            >
-                                                Editar
-                                            </Link>
-                                            <button
-                                                onClick={() => handleDelete(company)}
-                                                className="text-red-600 hover:text-red-900"
-                                            >
-                                                Eliminar
-                                            </button>
-                                        </td>
-                                    </tr>
-                                ))}
-                                </tbody>
-                            </table>
-                        </div>
+                        <Table
+                            routesKey="companies"
+                            rows={{
+                                nit:     'Nit',
+                                name:    'Nombre',
+                                address: 'Dirección',
+                                phone:   'Teléfono',
+                            }}
+                            values={companies}
+                        />
                     </div>
                 </div>
             </div>
---
+++ resources/js/Pages/Company/Partials/Table.jsx
@@ -0,0 +1,74 @@
+import {Link, useForm} from '@inertiajs/react';
+
+export default function Table({
+    routesKey,
+    rows,
+    values,
+}) {
+    const {delete: destroy} = useForm();
+
+    const handleDelete = async (value) => {
+        destroy(route(`${routesKey}.destroy`, value.id), {
+            preserveScroll: true,
+            onSuccess:      () => alert('Eliminado correctamente'),
+            onError:        () => () => alert('Ha ocurrido un error'),
+        });
+    };
+
+    const Td = ({children}) => (
+        <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+            {children}
+        </td>
+    );
+
+    const Th = ({children}) => (
+        <th
+            scope="col"
+            className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+        >
+            {children}
+        </th>
+    );
+
+    return (
+        <div className="mt-4 shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+            <table className="min-w-full divide-y divide-gray-200">
+                <thead className="bg-gray-50">
+                <tr>
+                    {Object.keys(rows).map((title, key) => <Th key={key}>{title}</Th>)}
+                    <Th/>
+                </tr>
+                </thead>
+                <tbody className="bg-white divide-y divide-gray-200">
+                {values.map(value => (
+                    <tr key={value.id}>
+                        {Object.keys(rows).map((rowKey, index) => (
+                            <Td key={index}>{value[rowKey]}</Td>
+                        ))}
+                        <Td>
+                            <Link
+                                href={route(`${routesKey}.show`, value.id)}
+                                className="text-blue-600 hover:text-blue-900 mr-2"
+                            >
+                                Ver
+                            </Link>
+                            <Link
+                                href={route(`${routesKey}.edit`, value.id)}
+                                className="text-green-600 hover:text-green-900 mr-2"
+                            >
+                                Editar
+                            </Link>
+                            <button
+                                onClick={() => handleDelete(value)}
+                                className="text-red-600 hover:text-red-900"
+                            >
+                                Eliminar
+                            </button>
+                        </Td>
+                    </tr>
+                ))}
+                </tbody>
+            </table>
+        </div>
+    );
+}
